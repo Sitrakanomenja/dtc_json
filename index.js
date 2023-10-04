1f581c345bee96e30d083a487fa0366f9bbd75ab
Sitrakanomenja <135508319+Sitrakanomenja@users.noreply.github.com>
2023-10-04 18:46:21 +0200

    deuxieme com

--- index.js
+++ index.js
@@ -1,70 +1,60 @@
-// afficher le country de l'index1 seulement
-// fetch("./sales_100.json")
-// .then((response) => response.json())
-// .then((data) => console.log(data.sales_100[0].country));
-// ---------------------------------------------------------------------------------------
+fetch("./sales_100.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const select = document.getElementById("item-chose");
+    const boite = document.getElementById("item_list");
+    const para = document.getElementById("paragraphe")
+    const regions = [...new Set(data.sales_100.map((user) => user.region))]; // Utilisez un ensemble (Set) pour obtenir des régions uniques.
 
-// Afficher toutes les country avec forEach()
-//  fetch("./sales_100.json")
-//    .then((response) => response.json())
-//    .then((data) => {
-//      data.sales_100.forEach((element) => {
-//        console.log(element.country);
-//      });
-//    });
-// -----------------------------------------------------------------------------------------
+    select.addEventListener("change", function () {
+      const valeur = select.value;
 
-//Afficher les régions avec filer()
-// fetch("./sales_100.json")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     data.sales_100.filter((element) => {
-
-//       console.log(element.order_id);
-//     });
-//   });
-// ------------------------------------------------------------------------------------------
-
-// Afficher avec map()
- fetch("./sales_100.json")
-   .then((response) => response.json())
-   .then((data) => {
-    document.body.innerHTML = data.sales_100.map((user) => 
+      if (valeur === "tous") {
+        boite.innerHTML = data.sales_100.map(
+          (user) =>
             `  <div class="container">
-                <h1> &#128578; &#128564;</h1> <h1>  </h1>
-                        <h2>${user.country} <br> ${user.region}</h2>
-                        <p>${user.unit_cost} ${user.units_sold}${user.units_sold}</p>
-                        <p>${user.item_type} <br> ${user.sales_channel}</p>
-                        <p>order_date: ${user.order_date} </p>
-                        <p>ship_date: ${user.ship_date} </p>
-                        <b> <i>${user.order_id}</i> </b><br>
-           
-                </div>
-            `
-    )
-
-   });
-// --------------------------------------------------------------------------------------------------
-
-
-// Lire les fichier csv
-
-// fetch("./nba.csv")
-//    .then((response) => response.text())
-//    .then((data) => {
-
-//     // Diviser le texte CSV en lignes
-//     const lines = data.split('\n');
-//       // Parcourir chaque ligne
-//       lines.forEach(line => {
-//         // Diviser la ligne en colonnes en utilisant la virgule comme délimiteur
-//         const columns = line.split(',');
-  
-//         console.log(columns);
-//       });
-//     })
-//     .catch(error => {
-//       console.error('Erreur lors de la lecture du fichier CSV:', error);
-//    });
-
-
+                    <h4> &#128578; &#128564;</h4> 
+                            <h5>${user.country} <br> ${user.region}</h5>
+                            <p>${user.unit_cost} ${user.units_sold}${user.units_sold}</p>
+                            <p>${user.item_type} <br> ${user.sales_channel}</p>
+                            <p>order_date: ${user.order_date} </p>
+                            <p>ship_date: ${user.ship_date} </p>
+                            <b> <i>${user.order_id}</i> </b><br>
+            
+                    </div>
+                `
+        );
+      } else if (valeur === "region") {
+        boite.innerHTML = regions
+          .map(
+            (region) =>
+              `<button class="btn" data-region="${region}">${region}</button>`
+          )
+          .join("");
+        const buttons = document.querySelectorAll(".btn");
+        buttons.forEach((button) => {
+          button.addEventListener("click", function () {
+            const region = this.getAttribute("data-region");
+            const regionDetails = data.sales_100.filter(
+              (user) => user.region === region
+            );
+            const detailsHTML = regionDetails.map(
+              (user) =>
+                `<div class="container">
+                            <h5> ${user.region}</h5>
+                            <p>Type d'article: ${user.item_type}</p>
+                            <p>Unité vendue: ${user.units_sold}</p>
+                            <p>Prix unitaire: ${user.unit_price}</p>
+                            <p>Cout unitaire: ${user.unit_cost}</p>
+                  </div>
+                `
+            );
+            para.innerHTML = detailsHTML.join("");
+          });
+        });
+      } else {
+        boite.innerHTML = "";
+        para.innerHTML = "";
+      }
+    });
+  });
