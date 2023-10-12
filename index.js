ec5363bc216f6f76526f3ab06bb6ac904c0730ff
Sitrakanomenja <135508319+Sitrakanomenja@users.noreply.github.com>
2023-10-12 11:44:13 +0200

    cing com

--- index.js
+++ index.js
@@ -1,86 +1,92 @@
-fetch("./sales_100.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const select = document.getElementById("item-chose");
-    const boite = document.getElementById("item_list");
-    const paragraphe = document.getElementById("paragraphe");
-    const regions = [...new Set(data.sales_100.map((element) => element.region))];
 
-    select.addEventListener("change", function () {
-      const valeur = select.value;
+ fetch("./sales_100.json")
+   .then((response) => response.json())
+   .then((data) => {
+     const select = document.getElementById("item-chose");
+     const boite = document.getElementById("item_list");
 
-      if (valeur === "tous") {
-      
-        boite.innerHTML = data.sales_100.map(
-                     (user) =>
-                       `  <div class="container">
-                               <h4> &#128578; &#128564;</h4> 
-                                       <h5>${user.country} <br> ${user.region}</h5>
-                                       <p>${user.unit_cost} ${user.units_sold}${user.units_sold}</p>
-                                       <p>${user.item_type} <br> ${user.sales_channel}</p>
-                                       <p>order_date: ${user.order_date} </p>
-                                       <p>ship_date: ${user.ship_date} </p>
-                                       <b> <i>${user.order_id}</i> </b><br>
-                      
-                          </div>
-                        `
-                   );
-                   paragraphe.innerHTML ="";
+     select.addEventListener("change", function () {
+       const valeur = select.value;
 
-      } else if (valeur === "region") {
-    
-        boite.innerHTML = regions
-          .map(
-            (region) =>
-              `<button class="btn" data-region="${region}">${region}</button>`
-          )
-          .join("");
-        const buttons = document.querySelectorAll(".btn");
-        buttons.forEach((button) => {
-          button.addEventListener("click", function () {
-            const region = this.getAttribute("data-region");
-            const regionDetails = data.sales_100.filter(
-              (element) => element.region === region
-            );
-            const somme = regionDetails.reduce(function (accumulateur, currentValue) {
-              return accumulateur + currentValue.units_sold;
-            }, 0); // Initialisez à zéro
-
-            // Affiche la somme dans le paragraphe
-            paragraphe.innerHTML = `Somme des unités vendues dans ${region}: ${somme}`
-          });
-        });
+       if (valeur === "tous") {
+         boite.innerHTML = data.sales_100.map(
+           (element) =>
+             `  <div class="container">
+                                <h4> &#128578; &#128564</h4> 
+                                        <h5>${element.country} <br> ${element.region}</h5>
+                                        <p>${element.unit_cost} ${element.units_sold}${element.units_sold}</p>
+                                        <p>${element.item_type} <br> ${element.sales_channel}</p>
+                                        <p>order_date: ${element.order_date} </p>
+                                        <p>ship_date: ${element.ship_date} </p>
+                                        <b> <i>${element.order_id}</i> </b><br>
+                                                 </div>
+                         `
+         )
+         .join("");
        
-      }else if(valeur === "histo"){
+       } else if (valeur === "region") {
+         const regions = [...new Set(data.sales_100.map((element) => element.region))];
+         const regionStatistics = [];
+
+         regions.forEach((region) => {
+           const regionDetails = data.sales_100.filter((element) => element.region === region);
+           const somme = regionDetails.reduce((accumulateur, currentValue) => accumulateur + currentValue.units_sold, 0);
+           regionStatistics.push({ region, somme });
+         });
+
+         const tableHTML = `
+           <table border="1">
+             <tr>
+               <th>Région</th>
+               <th>Somme des unités vendues</th>
+             </tr>
+             ${regionStatistics.map((statistic) => `
+               <tr>
+                 <td>${statistic.region}</td>
+                 <td>${statistic.somme}</td>
+               </tr>
+             `).join('')}
+           </table>
+         `;
+         boite.innerHTML = tableHTML;
         
+       } else if (valeur === "histo") {
+        const regions = [...new Set(data.sales_100.map((element) => element.region))];
+    
+        const sums = [];  // Tableau pour stocker les sommes par région
+    
+        regions.forEach((region) => {
+          const regionDetails = data.sales_100.filter((element) => element.region === region);
+          const sum = regionDetails.reduce((accumulateur, currentValue) => accumulateur + currentValue.units_sold, 0);
+          sums.push(sum); // Ajoutez la somme au tableau des sommes
+        });
+    
         //Model Bar
         const barCanvas = document.getElementById("barCanvas");
-
-        const barChart = new Chart(barCanvas,{
-            type : "bar" ,
-            data: {
-                labels : ["Sub-Saharan", "Australia and Oceania", "Middle East and North Africa", "Europe", "Asia", "North America" , "Central America and the Caribbean"],
-                datasets: [{
-                    data: [182870, 68325, 48678, 98117, 59967, 19143, 35771],
-                    backgroundColor: [
-                        "crimson","lightgreen", "lightblue", "violet", "yellow" ,"#56d798",
-                        "#ff8397",
-                    ]
-                }]
-            },
-       
-        })
-        boite.innerHTML = barChart;
-      
-
-      }
-       else {
+    
+        const barChart = new Chart(barCanvas, {
+          type: "bar",
+          data: {
+            labels: regions,
+            datasets: [
+              {
+                label: 'Unité de vente par région',
+                data: sums, 
+                backgroundColor: [
+                  "crimson",
+                  
+                ],
+              },
+            ],
+          },
+        });
           boite.innerHTML = "";
-          paragraphe.innerHTML = "";
-         
-      }
-    });
-  });
+       } else {
+         boite.innerHTML = "";
+        
 
+       }
+     });
+   });
 
-
+ 
